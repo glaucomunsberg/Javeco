a22ea6d6ceb3c6e05da7207f72baf4f0c18b2fce
Glauco Roberto Munsberg <glaucomunsberg@gmail.com>
2011-12-21 11:52:30 -0200

    Versão_0.4

--- automatico.sh
+++ automatico.sh
@@ -380,7 +380,22 @@ function git_remove()
 	read removendo
 	sudo git rm ${removendo}
 	echo "---------------------------------------------------------"
-	echo "- Commit para que que se delete os arquivos             -"
+	echo "- Commit para que se delete os arquivos                 -"
+	echo "---------------------------------------------------------"
+	read nada
+}
+
+function git_add()
+{
+	# Função que deleta os arquivos escolhidos nomeados pelo usuário
+	
+	clear
+	cabeca
+	echo "- Digite os arquivos que deseja adicionar:              -"
+	read adicionar
+	sudo git add ${adicionar}
+	echo "---------------------------------------------------------"
+	echo "- Commit para que adicione os arquivos             -"
 	echo "---------------------------------------------------------"
 	read nada
 }
@@ -422,11 +437,13 @@ function ferramentas()
 	echo "- 1. Git Remove                                         -"
 	echo "- 2. Git Status                                         -"
 	echo "- 3. Git Test                                           -"
+	echo "- 4. Git Add                                            -"
 	read nada
-	case $commit in
+	case $nada in
 		1) git_remove;;
 		2) git_status;;
 		3) git_test;;
+		4) git_add;;
 	esac
 }
 
